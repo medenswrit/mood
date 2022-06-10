06d0527d3be560cf33c9610b043120de3d363a5c
medenswrit <job@medens.org>
2022-06-10 19:01:19 -0400

    Adding a base alert dialog with accept and decline buttons

---
+++ lib/common/dialog/alert_dialog.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+Future<dynamic> showAlertDialog(
+  BuildContext context, {
+  required String title,
+  required String content,
+  required String cancelActionText,
+  required String defaultActionText,
+}) {
+  return showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: <Widget>[
+        TextButton(
+          child: Text(defaultActionText),
+          onPressed: () => Navigator.of(context).pop(true),
+        ),
+        TextButton(
+          child: Text(cancelActionText),
+          onPressed: () => Navigator.of(context).pop(false),
+        ),
+      ],
+    ),
+  );
+}
